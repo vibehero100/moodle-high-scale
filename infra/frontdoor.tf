a7f78369b8f7b8047de9ae3bda8fd199341ac494
vibehero100 <ridwan.suleiman@nnpcgroup.com>
2025-11-27 10:10:57 +0100

    Update frontdoor.tf

--- infra/frontdoor.tf
+++ infra/frontdoor.tf
@@ -1,17 +1,17 @@
 resource "azurerm_cdn_frontdoor_profile" "moodle-front-door" {
-  name                = "moodle-front-door"
+  name                = "FrontDoorNNPC"
   resource_group_name = data.azurerm_resource_group.moodle-high-scale.name
   sku_name            = "Premium_AzureFrontDoor"
 }
 
 resource "azurerm_cdn_frontdoor_endpoint" "moodle-front-door" {
-  name                     = "moodle-front-door-endpoint"
+  name                     = "Moodle-chqapp"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.moodle-front-door.id
 
 }
 
 resource "azurerm_cdn_frontdoor_origin_group" "moodle-front-door" {
-  name                     = "moodle-front-door"
+  name                     = "originGroup-Moodle-chqapp"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.moodle-front-door.id
 
   load_balancing {}
