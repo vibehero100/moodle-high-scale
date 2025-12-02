bd4a19798d484d539483342efdcd5b23f2213078
vibehero100 <ridwan.suleiman@nnpcgroup.com>
2025-12-02 09:39:40 +0100

    Update base.tf

--- infra/base.tf
+++ infra/base.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.85.0"
+    }
+  }
+}
 provider "azurerm" {
   features {}
   skip_provider_registration = true
@@ -33,3 +41,4 @@ data "azurerm_client_config" "current" {
   
 
 }
+
