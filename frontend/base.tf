26e465baf4f4bb120dc6095515a37aece2c99844
vibehero100 <ridwan.suleiman@nnpcgroup.com>
2026-02-11 12:55:40 +0100

    Update base.tf

--- frontend/base.tf
+++ frontend/base.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 3.88"
     }
   }
 }
@@ -38,6 +38,7 @@ data "azurerm_subnet" "app" {
   resource_group_name  = data.azurerm_resource_group.moodle-high-scale.name
 
 }
+
 
 
 
